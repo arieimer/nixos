7ae7d1b1057a2cefdddbe7b1a94aef75a1fce004
ari-rs <ari.eimer@proton.me>
2025-04-07 22:53:37 -0500

    installer bugs fixed

--- packages/installer/install-profile.sh
+++ packages/installer/install-profile.sh
@@ -8,13 +8,12 @@ fi
 
 git clone https://github.com/ari-rs/nixconfig.git "$HOME/nixconfig"
 
-TARGET_PROFILE=$(find . -mindepth 1 -maxdepth 1 -type d ! -name default -exec basename {} \; | gum choose)
+TARGET_PROFILE=$(find ~/nixconfig/hosts -mindepth 1 -maxdepth 1 -type d ! -name default -exec basename {} \; | gum choose)
 
 gum confirm  --default=false "This will install $TARGET_PROFILE please verify this is correct."
 
 echo "Starting install process..."
 
 if [ "$TARGET_PROFILE" == "detlas" ]; then
-  echo DETLAS INSTALL INIT
-  #sudo disko-install --flake .#detlas --disk main /dev/disk/by-id/nvme-WD_Blue_SN570_500GB_222622801561
+  sudo disko-install --flake nixconfig#detlas --disk main /dev/disk/by-id/nvme-WD_Blue_SN570_500GB_222622801561
 fi

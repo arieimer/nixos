3a5923cceea03cef1c0d6cafbe9cfbe4caa96e6a
ari <ari.eimer@proton.me>
2026-06-14 16:09:07 -0500

    fix(anki): purely state anki with hack fix for AJT

--- modules/nixos/desktop/programs/anki.nix
+++ modules/nixos/desktop/programs/anki.nix
@@ -5,7 +5,6 @@
   ...
 }: let
   inherit (lib) mkEnableOption mkIf;
-  addonPath = pkg: "${pkg}/share/anki/addons/${lib.removePrefix "anki-addon-" pkg.pname}";
 in {
   options.cfg.programs.anki.enable = mkEnableOption "anki";
   config = mkIf config.cfg.programs.anki.enable {
@@ -14,11 +13,11 @@ in {
       packages = [
         pkgs.anki
       ];
-      xdg.data.files = {
-        "Anki2/addons21/anki-connect".source = addonPath pkgs.ankiAddons.anki-connect;
-        "Anki2/addons21/review-heatmap".source = addonPath pkgs.ankiAddons.review-heatmap;
-        "Anki2/addons21/passfail2".source = addonPath pkgs.ankiAddons.passfail2;
-        # Rest must be state for now...
+      # Beyond a hacky fix for AJT mecab controller
+      xdg.data.files."Anki2/addons21/200813220/mecab_controller/support/mecab.lin" = {
+        type = "copy";
+        permissions = "755";
+        source = "${pkgs.mecab}/bin/mecab";
       };
     };
   };
